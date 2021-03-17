44af2c5e05bdf097a348a11cd4ae6c15393c2382
wade-peterson <wade.peterson@workday.com>
2021-03-16 19:59:48 -0600

    try things

--- webpack.base.config.js
+++ webpack.base.config.js
@@ -1,46 +1,61 @@
-const path = require('path');
-const webpack = require('webpack');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+// @ts-check
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
-const gameNames = ['baby-monkey', 'flappy-bird-cat', 'goat'];
+const gameConfigs = [
+  { key: "baby-monkey", title: "Baby Monkey" },
+  { key: "flappy-bird-cat", title: "Flappy Bird Cat" },
+  { key: "goat", title: "G.O.A.T." },
+];
 
 const entry = {};
-const plugins = [];
+const htmlPlugins = [];
 
-gameNames.forEach(gameName => {
-  entry[gameName] = path.join(__dirname, `src/games/${gameName}/index.ts`);
-  plugins.push(new HtmlWebpackPlugin({
-    chunks: [gameName],
-    filename: `${gameName}/index.html`,
-    template: path.join(__dirname, 'src/index.html')
-  }));
+gameConfigs.forEach(({ key, title }) => {
+  entry[key] = path.join(__dirname, `src/games/${key}/index.ts`);
+  htmlPlugins.push(
+    new HtmlWebpackPlugin({
+      chunks: [key],
+      filename: `${key}/index.html`,
+      template: path.join(__dirname, "src/game-template.html"),
+      title,
+    })
+  );
 });
 
 module.exports = {
   entry,
   output: {
-    path: path.join(__dirname, 'dist'),
-    filename: '[name].[hash].js'
+    path: path.join(__dirname, "dist"),
+    filename: "[name].[hash].js",
   },
   resolve: {
-    extensions: ['.js', '.ts'],
+    extensions: [".js", ".ts"],
     alias: {
-      pixi: path.join(__dirname, 'node_modules/phaser-ce/build/custom/pixi.js'),
-      phaser: path.join(__dirname, 'node_modules/phaser-ce/build/custom/phaser-split.js'),
-      p2: path.join(__dirname, 'node_modules/phaser-ce/build/custom/p2.js')
-    }
+      pixi: path.join(__dirname, "node_modules/phaser-ce/build/custom/pixi.js"),
+      phaser: path.join(
+        __dirname,
+        "node_modules/phaser-ce/build/custom/phaser-split.js"
+      ),
+      p2: path.join(__dirname, "node_modules/phaser-ce/build/custom/p2.js"),
+    },
   },
-  plugins,
+  plugins: [
+    ...htmlPlugins,
+  ],
   module: {
     rules: [
-      { test: /\.tsx?$/, use: 'awesome-typescript-loader' },
-      { test: /\.json$/, use: 'json-loader' },
-      { test: /\.html$/, use: 'html-loader' },
-      { test: /\.css$/, use: ['style-loader', 'css-loader'] },
-      { test: /(fonts|assets)(\/|\\)/, use: 'file-loader?name=assets/[hash].[ext]' },
-      { test: /pixi\.js$/, use: 'expose-loader?PIXI' },
-      { test: /phaser-split\.js$/, use: 'expose-loader?Phaser' },
-      { test: /p2\.js$/, use: 'expose-loader?p2' }
-    ]
-  }
+      { test: /\.tsx?$/, use: "awesome-typescript-loader" },
+      { test: /\.json$/, use: "json-loader" },
+      { test: /\.html$/, use: "html-loader" },
+      { test: /\.css$/, use: ["style-loader", "css-loader"] },
+      {
+        test: /(fonts|assets)(\/|\\)/,
+        use: "file-loader?name=/assets/[hash].[ext]",
+      },
+      { test: /pixi\.js$/, use: "expose-loader?PIXI" },
+      { test: /phaser-split\.js$/, use: "expose-loader?Phaser" },
+      { test: /p2\.js$/, use: "expose-loader?p2" },
+    ],
+  },
 };
